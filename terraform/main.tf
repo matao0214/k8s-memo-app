a68f63ebdba58d915044dff2239a62792c0d79f9
matao0214 <y.kawamata0214@gmail.com>
2024-09-22 15:53:40 +0900

    import artifact_registry_repository

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,3 +49,20 @@ resource "google_container_cluster" "default" {
   # accidentally delete this instance by use of Terraform.
   deletion_protection = false
 }
+
+resource "google_artifact_registry_repository" "default" {
+  repository_id = "docker"
+  location      = data.google_client_config.default.region
+  format        = "DOCKER"
+  cleanup_policies {
+    id     = "delete-old-image"
+    action = "KEEP"
+    most_recent_versions {
+      keep_count             = 1
+      package_name_prefixes = []
+    }
+  }
+  docker_config {
+    immutable_tags = false
+  }
+}
