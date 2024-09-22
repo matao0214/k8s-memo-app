8a76fff8c0b7a659db2e9901807c6ebf4e88619f
matao0214 <y.kawamata0214@gmail.com>
2024-09-22 15:31:45 +0900

    api first commit

---
+++ api/app/controllers/todos_controller.rb
@@ -0,0 +1,44 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: [:update, :destroy]
+
+  # GET /todos
+  def index
+    @todos = Todo.all.order(created_at: :desc)
+    render json: @todos
+  end
+
+  # POST /todos
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save!
+      render json: @todo, status: :created, location: @todo
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /todos/:id
+  def update
+    if @todo.update(todo_params)
+      render json: @todo
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /todos/:id
+  def destroy
+    @todo.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def todo_params
+      params.require(:todo).permit(:title, :completed)
+    end
+end
