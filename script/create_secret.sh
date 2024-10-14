85b9af926a2895504e72dc8f31da760839fa8fab
matao0214 <y.kawamata0214@gmail.com>
2024-10-14 15:31:24 +0900

    fix api deployment yaml

--- script/create_secret.sh
+++ script/create_secret.sh
@@ -9,7 +9,7 @@ cd ../..
 echo "POSTGRES_DB=$(echo $terraform_output | jq -r '.db_name.value')" > api/config/secret/demo.env
 echo "POSTGRES_USER=$(echo $terraform_output | jq -r '.db_user.value')" >> api/config/secret/demo.env
 echo "POSTGRES_PASSWORD=$(echo $terraform_output | jq -r '.db_user_password.value')" >> api/config/secret/demo.env
-echo "DB_CONNECTION_NAME=$(echo $terraform_output | jq -r '.db_connection_name.value')" >> api/config/secret/demo.env
+echo "INSTANCE_CONNECTION_NAME=$(echo $terraform_output | jq -r '.db_connection_name.value')" >> api/config/secret/demo.env
 echo "POSTGRES_HOST=127.0.0.1" >> api/config/secret/demo.env
 echo "SECRET_KEY_BASE=$(openssl rand -hex 64)" >> api/config/secret/demo.env
 
