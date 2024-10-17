dcedc76aa29e3f6bb12966e88b47584cee203c34
matao0214 <y.kawamata0214@gmail.com>
2024-10-17 15:53:22 +0900

    fix ref external IP

--- script/create_secret.sh
+++ script/create_secret.sh
@@ -5,6 +5,10 @@ cd terraform/prod
 terraform_output=$(terraform output -json)
 cd ../..
 
+# serviceをデプロイ
+kubectl apply -f ./k8s/service/api.yaml
+kubectl apply -f ./k8s/service/frontend.yaml
+
 # 環境変数の定義を作成
 echo "POSTGRES_DB=$(echo $terraform_output | jq -r '.db_name.value')" > api/config/secret/demo.env
 echo "POSTGRES_USER=$(echo $terraform_output | jq -r '.db_user_name.value')" >> api/config/secret/demo.env
@@ -12,6 +16,7 @@ echo "POSTGRES_PASSWORD=$(echo $terraform_output | jq -r '.db_user_password.valu
 echo "INSTANCE_CONNECTION_NAME=$(echo $terraform_output | jq -r '.db_connection_name.value')" >> api/config/secret/demo.env
 echo "POSTGRES_HOST=127.0.0.1" >> api/config/secret/demo.env
 echo "SECRET_KEY_BASE=$(openssl rand -hex 64)" >> api/config/secret/demo.env
+echo "CORS_ALLOWED_ORIGINS=http://$(kubectl get service frontend -o jsonpath='{.status.loadBalancer.ingress[0].ip}')" >> api/config/secret/demo.env
 
 # KubernetesのSecretを作成
 kubectl create secret generic api-secret --from-env-file ./api/config/secret/demo.env --dry-run=client -o yaml | kubectl apply -f -
