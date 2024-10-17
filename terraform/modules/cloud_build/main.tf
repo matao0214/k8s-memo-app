884b019f4b4eabb843736034d0dd461016dc27dc
matao0214 <y.kawamata0214@gmail.com>
2024-10-17 15:50:06 +0900

    terraform: cloudbuild repo

--- terraform/modules/cloud_build/main.tf
+++ terraform/modules/cloud_build/main.tf
@@ -19,11 +19,64 @@ resource "google_cloudbuild_trigger" "memo-app" {
   project         = var.project_id
   service_account = var.service_account
 
-  github {
-    owner = "matao0214"
-    name  = "memo-app"
+  repository_event_config {
+    repository = google_cloudbuildv2_repository.my-repository.id
     push {
-      branch = "^main$"
+      branch       = "^main$"
+      invert_regex = false
     }
   }
+}
+
+resource "google_secret_manager_secret" "github-token-secret" {
+  project   = var.project_id
+  secret_id = "github-token-secret"
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_version" "github-token-secret-version" {
+  secret      = google_secret_manager_secret.github-token-secret.id
+  secret_data = trimspace(file("../modules/cloud_build/my-github-token.txt"))
+}
+
+data "google_project" "my_project" {
+  project_id = var.project_id
+}
+
+data "google_iam_policy" "p4sa-secretAccessor" {
+  binding {
+    role = "roles/secretmanager.secretAccessor"
+
+    members = [
+      "serviceAccount:service-${data.google_project.my_project.number}@gcp-sa-cloudbuild.iam.gserviceaccount.com"
+    ]
+  }
+}
+
+resource "google_secret_manager_secret_iam_policy" "policy" {
+  secret_id   = google_secret_manager_secret.github-token-secret.secret_id
+  policy_data = data.google_iam_policy.p4sa-secretAccessor.policy_data
+}
+
+resource "google_cloudbuildv2_connection" "my-connection" {
+  project  = var.project_id
+  location = "asia-east1"
+  name     = "my-repo-connection"
+
+  github_config {
+    app_installation_id = tonumber(trimspace(file("../modules/cloud_build/my-github-app-installation-id.txt")))
+    authorizer_credential {
+      oauth_token_secret_version = google_secret_manager_secret_version.github-token-secret-version.id
+    }
+  }
+}
+
+resource "google_cloudbuildv2_repository" "my-repository" {
+  location          = "asia-east1"
+  name              = "memo-app"
+  parent_connection = google_cloudbuildv2_connection.my-connection.name
+  remote_uri        = trimspace(file("../modules/cloud_build/my-github-repo-url.txt"))
 }
--- terraform/modules/service_account/main.tf
+++ terraform/modules/service_account/main.tf
@@ -15,3 +15,29 @@ resource "google_project_iam_member" "cloud_build_admin" {
   role    = "roles/container.admin"
   member  = "serviceAccount:${google_service_account.cloud_build.email}"
 }
+
+resource "google_service_account" "cloud_sql_connect" {
+  account_id   = "cloudsql-connect"
+  display_name = "Cloud SQL Connect Service Account"
+}
+
+data "google_iam_policy" "cloud_sql_admin" {
+  binding {
+    role = "roles/cloudsql.admin"
+
+    members = [
+      "serviceAccount:${google_service_account.cloud_sql_connect.email}"
+    ]
+  }
+}
+
+data "google_iam_policy" "cloud_sql_proxy" {
+  binding {
+    role = "roles/iam.workloadIdentityUser"
+
+    # kubernetesのサービスアカウント(cloud-sql-proxy)に紐づける
+    members = [
+      "serviceAccount:${var.project_id}.svc.id.goog[default/cloud-sql-proxy]"
+    ]
+  }
+}
